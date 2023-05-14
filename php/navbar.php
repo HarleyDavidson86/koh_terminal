7e91059b6139fd5f58b85639b7a2c97a3cb3aab9
Dominik Sust <dominik.sust@googlemail.com>
2023-05-14 10:02:29 +0200

    made navigation responsive

--- php/navbar.php
+++ php/navbar.php
@@ -1,22 +1,23 @@
 <div class="container mt-4">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <h3 id="logo">
 					<a href="<?php echo Theme::siteUrl() ?>">
 						<?php echo $site->title() ?>
 					</a>
 				</h3>
             </div>
-            <div class="col-md-6">
-                <div class="list-group list-group-horizontal float-right">
+		</div>
+		<div class="row">
+            <div class="col-md-12">
+                <div class="d-flex flex-row flex-wrap mb-3 float-right">
 					<?php foreach ($staticContent as $staticPage): ?>
-						<ul>
+						<div class="p-2">
 							<a href="<?php echo $staticPage->permalink() ?>">
 								<?php echo $staticPage->title() ?>
 							</a>
-						</ul>
+					</div>
 					<?php endforeach ?>
-
                 </div>
             </div>
         </div>
